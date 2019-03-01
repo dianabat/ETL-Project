49038c649a547bdfe62a0d7128532654b2e7f1b6
Data Analyst 77002 <analyst77002@outlook.com>
2019-02-28 20:06:20 -0600

    obtaining Diana's latest

--- ETL Flask Code.py
+++ ETL Flask Code.py
@@ -13,24 +13,46 @@ from sqlalchemy import create_engine, func, inspect, desc
 from flask import Flask, jsonify
 
 # Input the route to the data SQL file
-engine = create_engine('sqlite:///Resources/______.sqlite')?check_same_thread=False
+engine = create_engine("sqlite://Resources/______.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
 
-
+YelpData = Base.classes.yelpdata
+Commercial = Base.classes.commercial
+HTownDemo = Base.classes.htowndemo
 session = Session(engine)
 Base.metadata.create_all(engine)
 
 app = Flask(__name__)
-
-@app.route("/api/v1.0/")
 
 @app.route("/")
 def welcome():
     """List all routes that are available."""
     return (
         f"The Available Routes are:<br/>"
+        f"/api/v1.0/Htown_Demographics<br/>"
+        f"/api/v1.0/Commercial_Data<br/>"
+        f"/api/v1.0/Yelp_Data<br/>"
     )
+
+@app.route("/api/v1.0/Htown_Demographics")
+def demographics():
+    HtownStats = session.query(HTownDemo.htowndemo).all()
+    return jsonify(HtownStats)
+
+@app.route("/api/v1.0/Commercial_Data")
+def commercial():
+    Locations = session.query(Commercial.commercial).all()
+    return jsonify(Locations)
+
+@app.route("/api/v1.0/Yelp_Data")
+def yelpdata():
+    Restaurants = session.query(YelpData.yelpdata).all()
+    return jsonify(Restaurants)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
